67936426d04d47f9381adda7a81faa9b0b472d22
okoth kongo <okothkongo@gmail.com>
2020-01-09 15:55:22 +0300

    updgrade rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-
-   PASSWORD_REGEX = /\A (?=.{8,}) (?=.*\d) (?=.*[a-z]) (?=.*[A-Z])(?=.*[[:^alnum:]])/x
+  PASSWORD_REGEX = /\A (?=.{8,}) (?=.*\d) (?=.*[a-z]) (?=.*[A-Z])(?=.*[[:^alnum:]])/x.freeze
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -10,14 +9,14 @@ class User < ApplicationRecord
   validates :last_name, presence: true, length: { maximum: 30 }
   validates :password_confirmation, presence: true
 
-validate :password_complexity
-  
-  def password_complexity   
+  validate :password_complexity
+
+  def password_complexity
     return if password.blank?
-    unless password =~  PASSWORD_REGEX
-       self.errors.delete(:password)
-    errors.add :password, 'Complexity requirement not met. Length should be at least 8 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+
+    unless password =~ PASSWORD_REGEX
+      errors.delete(:password)
+      errors.add :password, 'Complexity requirement not met. Length should be at least 8 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
     end
 end
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    first_name { "Jane" }
-    last_name {"Doe"}
-    email { "user@random.com" }
-    password {'Password@123'}
-    password_confirmation {'Password@123'}
+    first_name { 'Jane' }
+    last_name { 'Doe' }
+    email { 'user@random.com' }
+    password { 'Password@123' }
+    password_confirmation { 'Password@123' }
   end
 end
